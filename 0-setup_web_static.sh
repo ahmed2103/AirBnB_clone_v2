0cadeb228411b5fa39780cfdafa2e7d778bb594e
kali <kali@kali.kali>
2024-04-04 20:41:29 +0200

    no return

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,29 +7,28 @@ apt-get install nginx -y
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "<h1>this content is a test</h1>" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/  /data/web_static/current
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu /data/
 chgrp -R ubuntu /data/
 echo "
 server {
-	listen 80;
-	listen [::]:80 default_server;
-	add_header X-Served-By $HOSTNAME;
-	root   /var/www/html;
-	index  index.html index.htm;
-	location /hbnb_static {
-		alias /data/web_static/current/
-		index.html index.htm
-	}
-    	location /redirect_me {
-        	return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+    listen 80;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html index.htm;
     }
-    	error_page 404 /404.html;
-    	location /404 {
-      	root /var/www/html;
-      	internal;
+    location /redirect_me {
+        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
-}" > etc/sites-available/default
+    error_page 404 /404.html;
+    location /404 {
+        root /var/www/html;
+        internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 service nginx restart
-
