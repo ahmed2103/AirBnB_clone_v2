023f9f95de83a9cbaf68642690896b41e57d18ad
kali <kali@kali.kali>
2024-04-19 18:00:46 +0200

    no return

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,10 +3,12 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
 
-def index(strict_slashes=False):
+@app.route('/', strict_slashes=False)
+def index():
     """displays welcome message"""
     return "Hello HBNB!"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
