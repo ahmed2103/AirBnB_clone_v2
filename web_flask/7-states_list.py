1cdda4e0ab2f9f42cce7643f8b00daf12496b3f7
ahmed2103 <123449036+ahmed2103@users.noreply.github.com>
2024-04-21 20:04:44 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -4,22 +4,19 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """Renders the list of states in html templete"""
+    states = storage.all("State").values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def shutdown_session(exception):
     """close the sqlalchemy session"""
     storage.close()
-
-
-@app.route('/states_list')
-def states_list():
-    """Renders the list of states in html templete"""
-    states = storage.all("State").values()
-    return render_template('7-states_list.html', states=states)
 
 
 if __name__ == '__main__':
