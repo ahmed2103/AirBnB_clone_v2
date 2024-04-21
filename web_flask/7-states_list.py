e1abefbc5a0d46dc58d6620bd4b111f0a0c92bf5
Hasona <hasona@kali.kali>
2024-04-21 13:26:37 +0200

    test

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,6 +7,8 @@ from models.state import State
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
+
+
 @app.teardown_appcontext
 def shutdown_session(exception):
     """close the sqlalchemy session"""
@@ -19,6 +21,7 @@ def states_list():
     states_list = storage.all(State).values()
     sorted_states = sorted(states_list, key=lambda x: x.name)
     return render_template('7-states_list.html', states=sorted_states)
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
